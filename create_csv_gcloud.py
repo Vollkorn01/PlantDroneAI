547e3104c5aa8a8382cd7546c9335071b4c13f77
Alexander Pfyffer <alexander@Alexanders-MacBook-Pro.local>
2019-11-23 21:28:57 +0100

    added validation to lables

--- create_csv_gcloud.py
+++ create_csv_gcloud.py
@@ -11,46 +11,66 @@ import pandas as pd
 
 L  = []
 
-directory = "/Users/alexander/Downloads/data_224x224_small/train/bitter_dock"
+directory = "/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/train/bitter_dock"
 for filename in os.listdir(directory):
     print(filename)
     if filename.endswith(".jpg"):
-        L.append('gs://evident-minutia-180203-vcm/data_224x224_small/train/bitter_dock/' + filename + ',bitter_dock')
-        print('gs://evident-minutia-180203-vcm/data_224x224_small/train/bitter_dock/' + filename + ',bitter_dock')
+        L.append('TRAIN,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/train/bitter_dock/' + filename + ',bitter_dock')
+        print('TRAIN,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/train/bitter_dock/' + filename + ',bitter_dock')
         continue
     else:
         continue
 
-directory = "/Users/alexander/Downloads/data_224x224_small/train/other"
+directory = "/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/train/other"
 for filename in os.listdir(directory):
     print(filename)
     if filename.endswith(".jpg"):
-        L.append('gs://evident-minutia-180203-vcm/data_224x224_small/train/other/' + filename + ',other')
-        print('gs://evident-minutia-180203-vcm/data_224x224_small/train/other/' + filename + ',other')
+        L.append('TRAIN,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/train/other/' + filename + ',other')
+        print('TRAIN,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/train/other/' + filename + ',other')
         continue
     else:
         continue
 
-directory = "/Users/alexander/Downloads/data_224x224_small/test/bitter_dock"
+directory = "/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/test/bitter_dock"
 for filename in os.listdir(directory):
     print(filename)
     if filename.endswith(".jpg"):
-        L.append('gs://evident-minutia-180203-vcm/data_224x224_small/test/bitter_dock/' + filename + ',bitter_dock')
-        print('gs://evident-minutia-180203-vcm/data_224x224_small/test/bitter_dock/' + filename + ',bitter_dock')
+        L.append('TEST,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/test/bitter_dock/' + filename + ',bitter_dock')
+        print('TEST,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/test/bitter_dock/' + filename + ',bitter_dock')
         continue
     else:
         continue
 
-directory = "/Users/alexander/Downloads/data_224x224_small/test/other"
+directory = "/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/test/other"
 for filename in os.listdir(directory):
     print(filename)
     if filename.endswith(".jpg"):
-        L.append('gs://evident-minutia-180203-vcm/data_224x224_small/test/other/' + filename + ',other')
-        print('gs://evident-minutia-180203-vcm/data_224x224_small/test/other/' + filename + ',other')
+        L.append('TEST,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/test/other/' + filename + ',other')
+        print('TEST,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/test/other/' + filename + ',other')
+        continue
+    else:
+        continue
+    
+directory = "/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/validation/bitter_dock"
+for filename in os.listdir(directory):
+    print(filename)
+    if filename.endswith(".jpg"):
+        L.append('VALIDATION,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/validation/bitter_dock/' + filename + ',bitter_dock')
+        print('VALIDATION,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/validation/bitter_dock/' + filename + ',bitter_dock')
+        continue
+    else:
+        continue
+    
+directory = "/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/validation/other"
+for filename in os.listdir(directory):
+    print(filename)
+    if filename.endswith(".jpg"):
+        L.append('VALIDATION,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/validation/other/' + filename + ',other')
+        print('VALIDATION,' + 'gs://evident-minutia-180203-vcm/data_224x224_split/validation/other/' + filename + ',other')
         continue
     else:
         continue
 
 df = pd.DataFrame(L, columns=['[set,]image_path[,label]'])
 
-df.to_csv(r'/Users/alexander/Downloads/data_224x224_small/labels.csv', index = False, doublequote=False)
+df.to_csv(r'/Users/alexander/Documents/DevProjects/PlantDroneAI/data_224x224_split/labels.csv', index = False, doublequote=False)
